198efc212be1e26e8c5bfe0ae8e165b0e0e2e5da
bimalki <abyu629@gmail.com>
2020-03-31 00:48:12 +0900

    Initial commit

---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number =  5;
+let speed : string = 'fast'; 
+let hasname: boolean = true;
+
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+//ビルトインオブジェクト
+let now: Date = new Date();
+sw
